1d1ff38fdf8c1df46e03f2d323872cd80792fe2a
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-06-08 08:04:10 +0000

    boggle fails test

---
+++ boggle.py
@@ -0,0 +1,5 @@
+def make_grid(width, height):
+    """
+    Make an empty boggle grid
+    """
+    return {}
--- test_boggle.py
+++ test_boggle.py
@@ -1,8 +1,23 @@
 import unittest
+import boggle
 
 
-class test_boggle(unittest.TestCase):
-    def is_this_thing_on(self):
-        self.assertEqual(1,1)
+class TestBoggle(unittest.TestCase):
+    """
+    our test suite for boggle solver
+    """
+    
+    def test_can_create_an_empty_grid(self):
+        """
+        Test to see if we can create an empty grid
+        """
+        grid = boggle.make_grid(0,0)
+        self.assertEqual(len(grid),0)
         
-        
+    def test_grid_size_is_width_times_height(self):    
+        """
+        Test is to ensure that the total size of the grid 
+        is equal to width * height
+        """
+        grid = boggle.make_grid(2, 3)
+        self.assertEqual(len(grid), 6)
