e6b4c494aef29171ad373f01fb382e88096222c4
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-06-15 10:52:58 +0000

    Word in Dictionary

--- boggle.py
+++ boggle.py
@@ -56,16 +56,24 @@ def path_to_word(grid, path):
     """
     return ''.join([grid[p] for p in path])
     
+    
+    
+def word_in_dictionary(word, dict):
+    return word in dict
+    
+    
+    
 def search(grid, dictionary):
     """
-    Search through the paths to locate words by matching strings to words in a dictionary
+    Search through the paths to locate words by matching 
+    strings to words in a dictionary
     """
     neighbours = all_grid_neighbours(grid)
     paths = []
     
     def do_search(path):
         word = path_to_word(grid, path)
-        if word in dictionary:
+        if word_in_dictionary(word, dictionary):
             paths.append(path)
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
